71157fa118445ef0bdfaf7901cc3b07c506d267a
unknown <ejajul.masai@gmail.com>
2025-05-29 10:50:23 +0530

    JavaScript Day 3

---
+++ javascript/day-3/index.js
@@ -0,0 +1,9 @@
+let firstName = "Ejajul";
+let lastName = "Ansari";
+
+let output1 = firstName + " " + lastName;
+console.log(output1);
+
+
+let output2 = `${firstName} ${lastName}`;
+console.log(output2);
