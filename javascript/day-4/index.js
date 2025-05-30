0b954c7d7e8026eacff8ca3898c5c951f5136026
unknown <ejajul.masai@gmail.com>
2025-05-30 10:56:14 +0530

    JavaScript Day-4

---
+++ javascript/day-4/index.js
@@ -0,0 +1,14 @@
+// 4. Conditional Statements(if, else, ternary)
+// Create a program that checks if a person is eligible to vote using`if-else`,
+// and then optimize with the ternary operator.
+
+let age = 19;
+
+if (age >= 18) {
+    console.log("Eligible");
+} else {
+    console.log("Not Eligible");
+}
+
+age >= 18 ? console.log("Eligible") : console.log("Not Eligible");
+console.log(age >= 18 ? "Eligible" : "Not Eligible");
