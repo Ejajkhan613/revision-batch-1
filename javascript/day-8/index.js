bf3092687b6ff7b4c4b40dcdfd71b910c55e41ad
unknown <ejajul.masai@gmail.com>
2025-06-03 11:10:40 +0530

    JavaScript Day 8

---
+++ javascript/day-8/index.js
@@ -0,0 +1,16 @@
+// 1. Array Basics: CRUD operations
+// Perform create, read, update, delete operations on an array of strings.
+
+let arr = ["Hello", "Hi", "Hey", "Good Morning", "Good Afternoon", "Good Evening"];
+
+// Create
+arr.push( "Good Night")
+
+// Read
+arr[0];
+
+// Update
+arr[2] = "Hey There";
+
+// Delete
+arr.pop();
