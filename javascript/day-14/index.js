e5e3613fa384d7700ff57afeee6407d4fc58b2ea
unknown <ejajul.masai@gmail.com>
2025-06-09 22:23:41 +0530

    JavaScript Day 14

---
+++ javascript/day-14/index.js
@@ -0,0 +1,14 @@
+// 7. Theory Day: Difference between Primitive vs Reference types
+// Explain how assignment and copying work for objects vs primitives.
+
+
+// Primitive Types
+// These types are provided by the computer to do any task
+// Using these types we can make other new data types
+// Primitive data types are not referneced, instead the variable directly points to it
+
+// Reference Types
+// These types are not provided by computer
+// We create them using primitive data types
+// Reference types are whose reference can point by multiple variables and
+// if any variable changes the value all the other variables will have the same changed value
