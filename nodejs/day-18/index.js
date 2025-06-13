2f43daef40f4fc517addc640e056e01987fcd7c7
unknown <ejajul.masai@gmail.com>
2025-06-13 21:21:39 +0530

    NodeJS Day 18

---
+++ nodejs/day-18/index.js
@@ -0,0 +1,8 @@
+// 4. Process Object & Environment Variables
+// Task: Log `process.env`, `process.argv`, and exit codes.
+// Bonus: Pass arguments from CLI.
+
+console.log(process.env);
+console.log(process.argv);
+
+console.log(process.argv[2]);
